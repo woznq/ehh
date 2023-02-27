48deadac4b6975776dd9dd7ac5282bc7bd91a3ec
student <abc@gmail.com>
2023-02-27 17:17:27 +0100

    zadanie 1 a

--- svg_draw/src/Main.java
+++ svg_draw/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point p = new Point(2,4);
+        System.out.println(p.x);
     }
 }
---
+++ svg_draw/src/Point.java
@@ -0,0 +1,12 @@
+public class Point
+{
+    public float x = 0;
+    public float y = 0;
+
+    Point (float x, float y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+}
+
