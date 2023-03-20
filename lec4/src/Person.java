63058cada521c700f3d16c4951beb0d1567f6038
student <student@student.pl>
2023-03-20 16:52:51 +0100

    lab 4 zad 1

---
+++ lec4/src/Person.java
@@ -0,0 +1,79 @@
+import javax.swing.text.DateFormatter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Person implements Serializable {
+    private String name;
+    private LocalDate birth, death;
+    private Person parents[] = new Person[2];
+
+    public Person(String name, LocalDate birth) {
+        this(name, birth, null);
+    }
+
+    public Person(String name, LocalDate birth, LocalDate death) {
+        this.name = name;
+        this.birth = birth;
+        this.death = death;
+        try {
+            if (birth.isAfter(death)) {
+                throw new NegativeLifespanException(birth, death, "Possible time-space loophole.");
+            }
+        } catch (NullPointerException e) {}
+    }
+
+    public Person(String name, LocalDate birth, LocalDate death, Person parent1, Person parent2) throws IncestException {
+        this(name, birth, death);
+        parents[0] = parent1;
+        parents[1] = parent2;
+
+        checkForIncest();
+    }
+
+    public Person(String name, LocalDate birth, Person parent1, Person parent2) throws IncestException {
+        this(name, birth, null, parent1, parent2);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birth=" + birth +
+                ", death=" + death +
+                ", parents=" + Arrays.toString(parents) +
+                '}';
+    }
+
+    private void checkForIncest() throws IncestException {
+        if(parents[0] == null || parents[1] == null)
+            return;
+        for(var leftSideParent : parents[0].parents) {
+            if (leftSideParent == null) continue;
+            for (var rightSideParent : parents[1].parents) {
+                if (rightSideParent == null) continue;
+                if (leftSideParent == rightSideParent)
+                    throw new IncestException(leftSideParent, this);
+            }
+        }
+    }
+    public static Person loadPerson(String filePath) throws FileNotFoundException{
+        File file = new File(filePath);
+        Scanner s = new Scanner(file);
+        String name = s.nextLine();
+        LocalDate birth = LocalDate.parse(s.nextLine(), DateTimeFormatter.ofPattern("dd.MM.uuuu"));
+        String deathText = s.nextLine();
+        LocalDate death = null;
+        if (deathText !="") {
+            death = LocalDate.parse(deathText, DateTimeFormatter.ofPattern("dd.MM.uuuu"));
+        }
+        return new Person(name, birth, death);
+    }
+}
