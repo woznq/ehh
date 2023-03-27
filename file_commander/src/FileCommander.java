9431d6127829a94e7c64574d956772d87c0f2b3a
student <student@student.pl>
2023-03-27 17:30:24 +0200

    zad2

--- file_commander/src/FileCommander.java
+++ file_commander/src/FileCommander.java
@@ -1,4 +1,10 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileCommander {
     private Path path;
@@ -11,5 +17,26 @@ public class FileCommander {
     }
     public void cd(Path path){
         this.path = this.path.resolve(path).normalize();
+    }
+
+    public List<String> ls(){
+        Comparator<Path> comparator = (path2,path1)-> Boolean.compare(Files.isDirectory(path1),Files.isDirectory(path2));
+        comparator = comparator.thenComparing(Path::getFileName);
+        try {
+            return Files.list(path)
+                    //.map(Path::getFileName)
+                    .sorted(comparator)
+                    .map(o->{
+                        if (Files.isDirectory(o)) {
+                            return "["+o.getFileName().toString()+"]";
+                        }
+                        return o.getFileName().toString();
+                    })
+                    .collect(Collectors.toList());
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- file_commander/src/Main.java
+++ file_commander/src/Main.java
@@ -4,11 +4,6 @@ public class Main {
     public static void main(String[] args) {
         FileCommander fileCommander = new FileCommander();
         System.out.println(fileCommander.pwd());
-        fileCommander.cd(Path.of("/home/student/Pulpit"));
-        System.out.println(fileCommander.pwd());
-        fileCommander.cd(Path.of(".."));
-        System.out.println(fileCommander.pwd());
-        fileCommander.cd(Path.of("Dokumenty"));
-        System.out.println(fileCommander.pwd());
+        System.out.println(fileCommander.ls());
     }
 }
