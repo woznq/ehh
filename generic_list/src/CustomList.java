19c3d6f3399daa05202e7c926c116ff23426c72a
student <student@student.pl>
2023-04-03 17:11:54 +0200

    zad 1c

--- generic_list/src/CustomList.java
+++ generic_list/src/CustomList.java
@@ -1,43 +1,64 @@
-public class CustomList <T>{
-    private class Node{
+import java.util.NoSuchElementException;
+
+public class CustomList<T> {
+    private class Node {
         T value;
         Node next;
-        public Node(T value, Node next){
+
+        public Node(T value, Node next) {
             this.value = value;
             this.next = next;
         }
     }
-    private Node head=null, tail=null;
 
-    public void addLast(T value){
+    private Node head = null, tail = null;
+
+    public void addLast(T value) {
         Node node = new Node(value, null);
-        if(head==null){
+        if (head == null) {
             head = node;
-        }else if(head==tail){
+        } else if (head == tail) {
             head.next = node;
-        }else {
+        } else {
             tail.next = node;
         }
         tail = node;
     }
 
-    public void addFirst(T value){
+    public void addFirst(T value) {
         Node node = new Node(value, null);
-        if(head == null){
+        if (head == null) {
             head = node;
             tail = node;
-        }
-        else{
+        } else {
             node.next = head;
             head = node;
         }
     }
 
-    public T getFirst(){
+    public T getFirst() {
         return head.value;
     }
-    public T getLast(){
+
+    public T getLast() {
         return tail.value;
+    }
+
+    public T removeFirst() {
+        if (head==null){
+            throw new NoSuchElementException();
+        } else if (head==tail) {
+            Node temp = head;
+            head = null;
+            tail = null;
+            return temp.value;
+        }else{
+            Node temp = head;
+            head = head.next;
+            return temp.value;
+
+        }
+
     }
 
 
--- generic_list/src/Main.java
+++ generic_list/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CustomList<String> list = new CustomList<>();
-        list.addLast("abc");
         list.addLast("def");
+        list.addLast("ghi");
+        System.out.println(list.removeFirst());
         System.out.println(list.getFirst());
-        list.addFirst("zxc");
-        System.out.println(list.getFirst());
+//        list.addFirst("zxc");
+//        System.out.println(list.getFirst());
     }
 }
