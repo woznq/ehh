cc311427ba051b9c814d7e1b21a90417d6f6c166
student <student@student.pl>
2023-04-17 16:07:22 +0200

    lab 7

---
+++ lec7/shop/src/main/java/pl/umcs/oop/lec7/shop/Product.java
@@ -0,0 +1,13 @@
+package pl.umcs.oop.lec7.shop;
+
+import java.math.BigDecimal;
+
+public class Product {
+    protected String name;
+    protected BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+}
